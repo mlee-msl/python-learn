fe7dfd0b1fec93a257c68db7e4fdf7bd70861266
MLee <mleewhs@gmail.com>
2017-07-21 21:17:27 +0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-learn
+
+This repository is a sample tutorial for PYTHON incluing some docs which describe python-tips & some samples which from basics to improvement about varieties of aspects.
---
+++ src/filter-zip-map-reduce.py
@@ -0,0 +1,40 @@
+﻿#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"
+This module test four functions about filter & zip & map & reduce
+"
+
+if __name__ == '__main__':
+	print(__doc__)
+	#############################################################
+	#					filter
+	#############################################################
+	l1 = list(filter(lambda x : x % 2, range(50)))
+	print('filter: %s' % l1)
+
+	#############################################################
+	#					zip
+	#############################################################
+	l2 = list(zip(range(1, 50, 3), range(1, 100, 2)))
+	print('zip: %s' % l2)
+
+	#############################################################
+	#					map
+	#############################################################
+	l3 = list(map(lambda x, y, z : x + 2* y + 3 * z, range(10), range(10, 30, 2), range(2, 12))) # 序列长度要求一样，依次对应传给x,y,z
+	print('map: %s' % l3)
+
+	#############################################################
+	#					reduce
+	#############################################################
+	from functools import reduce
+	l4 = reduce(lambda x, y : x + 2* y, range(100))
+	print('reduce: %s' % l4)
+
+	l5 = reduce(lambda x, y : 10 * x + y, range(1,10))
+	print('reduce: %s' % l5)
+
+	l6 = int(reduce(lambda x, y : str(x) + str(y), range(1,10)))
+	print('reduce: %d' % l6)
+
