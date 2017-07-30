20e40dab3ec3e8255c1d08923d283519370dd47a
MLee <mleewhs@gmail.com>
2017-07-30 12:24:49 +0800

    modify prime

--- src/prime.py
+++ src/prime.py
@@ -5,29 +5,22 @@
 prime generator
 '''
 
-def odds():
-	'generate odd series which begins with 3'
-	n = 3
+def naturalNumbers():
+	'generate natural numbers series which begins with 2'
+	n = 2
 	while True:
 		yield n
-		n += 2
+		n += 1
+
+def process(n):
+	return lambda x : x % n != 0
 
 def primes():
 	'generator: a series of primes'
-	yield 2 # 2 of the first prime
-	g = odds()
+	g = naturalNumbers() # initialize series
 	while True:
-		n = next(g)
+		n = next(g) # get the first num of current series
 		yield n # return the first num of current series 
-		g = filter(lambda x : x % n != 0, g)
-
-def main(limit=10):
-	for i in primes():
-		if i < limit:
-			yield i
-		else:
-			break
-
-if __name__ == '__main__':
-	limit = int(input('Please type max value:'))
-	main(limit)
+		# adjust current series as new series by the Sieve of Eratosthenes
+#		g = filter(lambda x : x % n > 0, g) # method 1
+		g = filter(process(n), g) # method 2
